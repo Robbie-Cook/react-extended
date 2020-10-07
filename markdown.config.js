407114a89215aa2f4ff379feed4719cf98b73fb5
Robbie Cook <robbieck7@gmail.com>
2020-10-07 23:32:38 +1300

    Using tsdoc-extended to generate api

--- markdown.config.js
+++ markdown.config.js
@@ -1,61 +1,12 @@
 const fs = require("fs");
-const { NodeExtended } = require("node-extended");
-
-// Capitalize a string
-function toCapitalCase(str) {
-  if (!str || str === "") {
-    return str;
-  }
-  return str[0].toUpperCase() + str.slice(1);
-}
+const { TsDocExtended } = require("tsdoc-extended");
 
 /* CLI markdown.config.js file example */
 module.exports = {
   transforms: {
     /* Match <!-- AUTO-GENERATED-CONTENT:START (API) --> */
     API(content, options) {
-      NodeExtended.executeSync(`npx typedoc src/ --json docs/index.json`);
-      const data = JSON.parse(fs.readFileSync("./docs/index.json").toString());
-
-      let stringItems = [];
-      data.children.forEach((item) => {
-        if (item.name !== '"index"') {
-          item.children.forEach((child) => {
-            const name = child.name.replace(/"/g, "");
-            if (child.comment && !child.name.includes("Props")) {
-              stringItems.push(`* **${name}**`);
-
-              const subItems = [];
-              if (child.comment.shortText) {
-                subItems.push(`\t${child.comment.shortText}`);
-              }
-
-              if (child.comment.text) {
-                subItems.push(`${child.comment.text}`);
-              }
-
-              if (child.comment.tags) {
-                subItems.push("");
-                const item = `${child.comment.tags.map(
-                  (tag) =>
-                    `${toCapitalCase(tag.tag)}: ${tag.text.replace(
-                      /\n/g,
-                      "\n\t"
-                    )}`
-                )}`;
-                subItems.push(item);
-              }
-
-              stringItems.push(subItems.join("\n\t"));
-            }
-          });
-        }
-      });
-
-      return stringItems.join("\n");
-
-      // const string = fs.readFileSync('./docs/modules/.md');
-      // return string;
+      return TsDocExtended.generateMarkdown("src", ["index"]);
     },
   },
   callback: function () {
