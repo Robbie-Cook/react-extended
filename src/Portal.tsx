e7979b3c1e9fe1bdbdd94351af4303ea6afccd02
Robbie Cook <robbieck7@gmail.com>
2020-10-07 20:39:48 +1300

    Added Portal component

---
+++ src/Portal.tsx
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React, { ReactNode } from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * Interface for Portal props
+ */
+export interface PortalProps {
+  children?: ReactNode;
+}
+
+/**
+ * Make a portal in the DOM.
+ * 
+ * TODO: add a11y support
+ */
+const Portal = (props: PortalProps) => {
+  ReactDOM.createPortal(props.children, document.body);
+};
+
+export default Portal;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.ts"),
+  entry: path.resolve(__dirname, "./src/index.tsx"),
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index.js",
